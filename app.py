db34e4afa5d5f9e06ef8f7946c1732c78ed95ee4
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2017-04-27 22:14:26 -0400

    removed endpoint.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from hyper2web import app, endpoint
+from hyper2web import app
 
 if __name__ == '__main__':
 
 	# A basic callback style API is provided
-	async def get_name(handler: endpoint.EndPointHandler):
-		print('data received', str(handler.stream.data, encoding='utf8'))
-		await handler.send_and_end(str(handler.stream.data, encoding='utf8'))
+	async def get_name(http, stream):
+		# this route essentially echo the data received back to client
+		print('data received:')
+		print(str(stream.data, encoding='utf8'))
+		await http.send_and_end(stream, stream.data)
 
 	app = app.App(static_file_handle='auto', root_route='index.html')
 	app.post('name', get_name)

071f11aee7593d09641c9222124689e81acce6d7
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2017-05-23 00:43:59 -0400

    + setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name="hyper2web",
+	version="0.0.0",
+	description="Super Fast H2 Backend Framework for Progressive Web App",
+	url="https://github.com/CreatCodeBuild/hyper2web",
+	author="CreatCodeBuild@github.com",
+	package=['hyper2web'],
+	install_requires=['h2', 'curio']
+)

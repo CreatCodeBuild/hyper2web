8d8da43bf8b293305439a460111716e84c6a839e
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2017-08-19 03:39:37 -0700

    update import

--- hyper2web/__init__.py
+++ hyper2web/__init__.py
@@ -1,1 +1,5 @@
+__author__ = """Xuanzhe Wang"""
+__email__ = 'wangxuanzhealbert@gmail.com'
+__version__ = '0.0.3'
+
 from . import app
--- hyper2web/_info_.py
+++
@@ -1,3 +0,0 @@
-__author__ = """Xuanzhe Wang"""
-__email__ = 'wangxuanzhealbert@gmail.com'
-__version__ = '0.0.3'
